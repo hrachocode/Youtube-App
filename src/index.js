11516aad820fabd5444ef45968eabd31896b3e23
hrachocode <hracho94@yahoo.com>
2019-03-14 17:24:27 +0400

    Initialized youtube api connection

--- src/index.js
+++ src/index.js
@@ -1,22 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// API request component
+import youtube from './api/youtube';
+
 class App extends React.Component {
 
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			q: 'surfing',
+			videos: []
+		}
+	}
+
+	getRequest = () => {	
+		youtube.get('/youtube/v3/search', {
+			params: {
+				q: this.state.q,
+				part: 'snippet',
+				key: 'AIzaSyBV2a7cUURIG4h1mhkLuevGms445b0dkLo'
+			}
+		}).then(response => {
+			let videos = response.data.items;
+			this.setState({
+				videos
+			})
+		})
+		.catch(error => console.log(error))
+	}
+
+	componentDidMount() {
+		this.getRequest();
 	}
 
 	render() {
+		
 		return (
 			<div>
-				<ul>
-					<li>1</li>
-					<li>2</li>
-					<li>3</li>
-					<li>4</li>
-					<li>5</li>
-				</ul>
+				{/*  */}
 			</div>
 		);
 	}
